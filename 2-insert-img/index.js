ed85ce0e110a15009d95ee799097f6914533a6fa
Jordan Pratt <jpratt@mtroyal.ca>
2023-02-28 09:16:51 -0700

    Initial commit

---
+++ 2-insert-img/index.js
@@ -0,0 +1,4 @@
+import { picData } from "./data/pic-data.js";
+import { picsumImg } from "./helpers/picsum-helpers.js";
+
+document.querySelector("div").append(img);
---
+++ 3-styling/index.js
@@ -0,0 +1,3 @@
+let img = document.createElement("img");
+img.src = `https://picsum.photos/id/${picData.id}/${picData.width}`;
+document.querySelector("div").append(img);
